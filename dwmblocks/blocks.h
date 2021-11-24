49d5a568fad695c5198c0a9151ef4285bccea45d
bajelidze <irakli.bajelidze@exasol.com>
2021-11-24 22:37:18 +0100

    New blocks config

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,13 +1,10 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	// {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
     { "", "bash weather | sed 's/\\..*°/°/'", 300, 0 },
+    { "TEMP: ", "sensors | awk '/Core 0:/ {print $3}' | sed 's/+//;s/\\.0//'", 3, 0 },
     { "BAT: ", "battery", 5, 0 },
     { "BRIT: ", "bc <<< \"$(xrandr --prop --verbose | sed -n '/Brightness:/s/.*:\\ //p')*100\" | sed 's/\\..*//'", 0, 12 },
     { "VOL: ", "volumestatus", 0, 10 },
-    { "TEMP: ", "sensors | awk '/Core 0:/ {print $3}' | sed 's/+//;s/\\.0//'", 3, 0 },
     { "", "klayout", 0, 11 },
 	{ "", "date '+%b %d (%a) %H:%M:%S'", 1, 0 }
 };
