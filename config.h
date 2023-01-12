33b37dd3da1e66469adf015300730198bcb709fa
bajelidze <irakli.bajelidze@exasol.com>
2023-01-12 22:37:32 +0100

    Push latest

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Weather Icons:size=11", "Iosevka Nerd Font:size=11" };
+static const char *fonts[]          = { "Weather Icons:size=13", "Iosevka Nerd Font:size=13" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#1e1e1e";
 static const char col_gray2[]       = "#444444";
@@ -86,7 +86,7 @@ static const char *downvol[] = { "volumectl", "-d", "5", NULL };
 static const char *mutevol[] = { "volumectl", "-m",  NULL };
 static const char *upbrit[]   = { "brightness", "-inc", "10", NULL };
 static const char *downbrit[] = { "brightness", "-dec", "10", NULL };
-static const char *chrome[]  = { "google-chrome-stable", "--password-store=gnome", NULL };
+static const char *chrome[]  = { "google-chrome-scaled", "--password-store=gnome", NULL };
 // static const char *firefox[]  = { "firefox", NULL };
 // static const char *kswitch[]  = { "klswitch", NULL };
 static const char *gnomecontrolcenter[]  = { "gnome-control-center", NULL };
